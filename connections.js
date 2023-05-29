a1d6d669c406c976b90ea5bfe14e652318914a3f
OMAR CORRALES <omarcorrales@MacBook-Pro-de-OMAR.local>
2023-05-29 16:09:06 -0500

    se actualizo la conexion

--- connections.js
+++ connections.js
@@ -1,14 +1,14 @@
 
 const admin = require('firebase-admin');
-const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
-const { getAuth } = require('firebase-admin/auth');
+const { getFirestore } = require('firebase-admin/lib/firestore');
+const { getAuth } = require('firebase-admin/lib/auth');
 
 const serviceAccount = require("./goldendemo-22081-firebase-adminsdk-i2oub-e677da7c1e.json");
 
 const app = admin.initializeApp({ credential: admin.credential.cert(serviceAccount)});
 //const db = app.firestore();
 // const app = firebaseInit2.firestore();
-let db = getFirestore();
-let autenticacion = getAuth();
+let db = admin.firestore();
+let autenticacion = admin.auth();
 console.log('Firebase configurado');
 module.exports = {db, autenticacion};
